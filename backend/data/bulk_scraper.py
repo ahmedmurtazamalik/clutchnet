1e729af99c67ad8c3a2f351f3a265f08f2f73485
ahmedmurtazamalik <ahmedmurtazamalik@gmail.com>
2026-06-24 06:44:02 +0500

    update season list

--- backend/data/bulk_scraper.py
+++ backend/data/bulk_scraper.py
@@ -6,7 +6,7 @@ from backend.data.scraper import fetch_season_games, fetch_game_play_by_play, ge
 
 # Target Seasons from 2017-18 through 2024-25
 SEASONS = [
-    "2017-18", "2018-19", "2019-20", "2020-21", "2021-22",
+    "2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22",
     "2022-23", "2023-24", "2024-25"
 ]
 
